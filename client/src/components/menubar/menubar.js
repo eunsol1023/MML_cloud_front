6e3f8ae71f782b891e4928f59a69dd3fadaf43ed
eunsol1023 <dmsthf3055@naver.com>
2023-11-21 21:14:54 +0900

    Update menubar.js

--- client/src/components/menubar/menubar.js
+++ client/src/components/menubar/menubar.js
@@ -7,7 +7,7 @@ import axios from "axios";
 function getCsrfToken() {
   const cookies = document.cookie.split('; ');
   const csrfCookie = cookies.find(cookie => cookie.startsWith('csrftoken='));
-  return csrfCookie ? csrfCookie.split('=')[1] : null;
+  return csrfCookie ? decodeURIComponent(csrfCookie.split('=')[1]) : null;
 }
 
 function MenuBar() {
