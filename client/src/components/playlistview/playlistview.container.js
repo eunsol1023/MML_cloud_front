4f8191c4499dfbe7c69142973ab5a14feff398c7
eunsol1023 <dmsthf3055@naver.com>
2023-11-28 17:15:23 +0900

    Update playlistview.container.js

--- client/src/components/playlistview/playlistview.container.js
+++ client/src/components/playlistview/playlistview.container.js
@@ -3,32 +3,34 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-
 export default function Playlist() {
   const navigate = useNavigate();
 
   const [data, setData] = useState([]);
-  const [playTitle, setPlayTitle] = useState('');
-  
+  const [playTitle, setPlayTitle] = useState("");
+
   const onClickTitle = (title, artist) => {
-    localStorage.setItem('songtitle', title)
-    localStorage.setItem('songartist', artist)
+    localStorage.setItem("songtitle", title);
+    localStorage.setItem("songartist", artist);
     navigate("/songinfo");
   };
 
   const fetchData = async () => {
     if (localStorage.getItem("pagetype") === "search") {
       try {
-        const sentence = localStorage.getItem('sentence')
+        const sentence = localStorage.getItem("sentence");
         const response = await axios.get(
-          `http://api.cloudmml.com:8000/music/tag_song2vec/?input_sentence=${sentence}`
+          `http://api.cloudmml.com:8000/music/tag_song2vec/?input_sentence=${sentence}`,
+          {
+            withCredentials: true
+          }
         );
-        console.log(response)
+        console.log(response);
 
         if (response.status === 200) {
           console.log("전송성공");
           setData(response.data);
-          setPlayTitle(`'${sentence}' 기반 플레이리스트`)
+          setPlayTitle(`'${sentence}' 기반 플레이리스트`);
         } else if (response.status === 400) {
           console.log("Bad Request");
           return;
@@ -39,36 +41,14 @@ export default function Playlist() {
       } catch (error) {
         console.log(error);
       }
-
     } else if (localStorage.getItem("pagetype") === "song2vec") {
-
       const playlistTitle = "Song2vec으로 추천된 플레이리스트";
       try {
         const response = await axios.get(
           "http://api.cloudmml.com:8000/music/song2vec/",
-          {}
-        );
-
-        if (response.status === 200) {
-          console.log("전송성공");
-          setData(response.data);
-          setPlayTitle(playlistTitle);
-        } else if (response.status === 500) {
-            console.log("Internet Server Error");
-        } else {
-            console.log("Server Error");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-
-    } else if (localStorage.getItem("pagetype") === "user_like_artist") {
-
-      const playlistTitle = "사용자 협업 필터링으로 추천된 플레이리스트";
-      try {
-        const response = await axios.get(
-          "http://api.cloudmml.com:8000/music/user_like_artist/",
-          {withCredentials: true,}
+          {
+            withCredentials: true
+          }
         );
 
         if (response.status === 200) {
@@ -78,7 +58,27 @@ export default function Playlist() {
         } else if (response.status === 500) {
           console.log("Internet Server Error");
         } else {
-            console.log("Server Error");
+          console.log("Server Error");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else if (localStorage.getItem("pagetype") === "user_like_artist") {
+      const playlistTitle = "사용자 협업 필터링으로 추천된 플레이리스트";
+      try {
+        const response = await axios.get(
+          "http://api.cloudmml.com:8000/music/user_like_artist/",
+          { withCredentials: true }
+        );
+
+        if (response.status === 200) {
+          console.log("전송성공");
+          setData(response.data);
+          setPlayTitle(playlistTitle);
+        } else if (response.status === 500) {
+          console.log("Internet Server Error");
+        } else {
+          console.log("Server Error");
         }
       } catch (error) {
         console.log(error);
@@ -90,9 +90,11 @@ export default function Playlist() {
     fetchData();
   }, []);
 
-  return <PlayListViewUI 
-    onClickTitle={onClickTitle} 
-    data={data}
-    playTitle = {playTitle} 
-    />;
+  return (
+    <PlayListViewUI
+      onClickTitle={onClickTitle}
+      data={data}
+      playTitle={playTitle}
+    />
+  );
 }
